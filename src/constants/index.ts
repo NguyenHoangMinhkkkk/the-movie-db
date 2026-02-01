c7a28d84957e54c6dedc503b7c708f34e5dbe122
minh.nguyen <minh.nguyen@ilotusland.com>
2026-02-01 18:47:36 +0700

    feat: Clear sensitive information by resetting API constants in index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,14 @@
 export { default as StorageKey } from './StorageKey';
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const MEDIA_URL = 'https://image.tmdb.org/t/p/';
-const ACCESS_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTAxNTk1OTA0MTFkMTcxNzNjNGVlZjBlODViYzJmZiIsIm5iZiI6MTc2OTg1ODk5NC4xODU5OTk5LCJzdWIiOiI2OTdkZTdiMjE0ZGQ3YjMyMGFjZmUzMjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TbZkioTzlbnYbIQL4GFrI0hvxd6Gm1pQB8kSnE_ZgK4';
+
+const BASE_URL = '';
+const MEDIA_URL = '';
+const ACCESS_TOKEN = '';
 
 export const DEFAULT = {
   baseUrl: BASE_URL,
   mediaUrl: MEDIA_URL,
   accessToken: ACCESS_TOKEN,
-  accountId: 22722588,
+  accountId: 0,
   region: 'US',
   language: 'en-US',
 };
